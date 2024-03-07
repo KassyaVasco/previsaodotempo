8f7430b2798657121864cdf497507a7f64b4c613
KassyaVasco <kassyavasco@yahoo.com>
2024-03-07 15:37:12 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+let chave = "cebcd482eda57fa9a6714c1c2ba91885"
+
+
+function colocarNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    
+}
+
+    document.addEventListener("keypress", function(e) {
+
+        if(e.key === "Enter") {
+
+        const btn = document.querySelector (".botao-busca")
+
+        btn.click();
+        }
+    });
+
+async function buscarCidade(cidade){
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    cidade + 
+    "&appid=" + 
+    chave + 
+    "&lang=pt_br" +
+    "&units=metric"
+    )
+    .then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+
+function cliqueiNoBotao(){
+   let cidade = document.querySelector(".input-cidade").value
+
+   buscarCidade(cidade)
+}
